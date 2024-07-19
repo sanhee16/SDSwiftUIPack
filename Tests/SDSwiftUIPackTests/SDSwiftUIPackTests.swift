19dda4c671368de5aaba7806039fdeadecddf279
sanhee16 <sinhioa20@gmail.com>
2024-07-19 09:12:23 +0900

    Initial Commit

---
+++ Tests/SDSwiftUIPackTests/SDSwiftUIPackTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SDSwiftUIPack
+
+final class SDSwiftUIPackTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
